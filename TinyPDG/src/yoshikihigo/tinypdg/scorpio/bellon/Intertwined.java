d52af8e2b7f1c67212af5caa5d53443698dc221f
YoshikiHigo <yoshiki.higo@gmail.com>
2013-07-08 10:17:10 +0900

    Intertwined.jaca was modified. This file is used for extracting intertwined clones from a result of Scorpio.

--- TinyPDG/src/yoshikihigo/tinypdg/scorpio/bellon/Intertwined.java
+++ TinyPDG/src/yoshikihigo/tinypdg/scorpio/bellon/Intertwined.java
@@ -1,45 +1,65 @@
 package yoshikihigo.tinypdg.scorpio.bellon;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class Intertwined {
 
 	public static void main(final String[] args) {
 
-		if (1 != args.length) {
+		if (2 != args.length) {
 			System.err.println("the number of command opetions must be one.");
-			System.err.println("the first one is an output file of SCORPIO");
+			System.err.println("the first one is an output file of SCORPIO.");
+			System.err
+					.println("the second one is a file for storing intertwined clones.");
 			System.exit(0);
 		}
 
 		final String input = args[0];
+		final String output = args[1];
 
 		final List<ClonePairInfo> clonepairs = ClonePairInfo.getClonepairs(
 				new File(input), 6, false);
 
-		for (final ClonePairInfo pair : clonepairs) {
+		try {
 
-			final CodeFragmentInfo left = pair.left;
-			final CodeFragmentInfo right = pair.right;
+			final BufferedWriter writer = new BufferedWriter(new FileWriter(
+					output));
 
-			if (left.path.equals(right.path)) {
+			for (final ClonePairInfo pair : clonepairs) {
 
-				final int leftStart = left.first();
-				final int leftEnd = left.last();
-				final int rightStart = right.first();
-				final int rightEnd = right.last();
+				final CodeFragmentInfo left = pair.left;
+				final CodeFragmentInfo right = pair.right;
 
-				if ((leftStart < rightStart) && (leftEnd < rightStart)) {
-					continue;
-				}
+				if (left.path.equals(right.path)) {
 
-				if ((rightStart < leftStart) && (rightEnd < leftStart)) {
-					continue;
+					final int leftStart = left.first();
+					final int leftEnd = left.last();
+					final int rightStart = right.first();
+					final int rightEnd = right.last();
+
+					if ((leftStart < rightStart) && (leftEnd < rightStart)) {
+						continue;
+					}
+
+					if ((rightStart < leftStart) && (rightEnd < leftStart)) {
+						continue;
+					}
+
+					System.out.println(pair.toString());
+					writer.write(pair.toString());
+					writer.newLine();
 				}
 			}
 
-			System.out.println(pair.toString());
+			writer.flush();
+			writer.close();
+
+		} catch (final IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
